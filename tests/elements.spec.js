05f681092117e5ecfec8b182dc45e7c59724d8f9
thompsonjonm <thompson.jonm@gmail.com>
2021-01-19 19:48:56 -0500

    Add error and buttons tests

--- tests/elements.spec.js
+++ tests/elements.spec.js
@@ -1,12 +1,6 @@
 const { chromium } = require('playwright');
 
 let browser, page;
-const user = {
-  name: 'Test Tester',
-  email: 'test@test.com',
-  currentAddress: '3930 N Pine Grove Ave, Chicago, IL 60613',
-  permanentAddress: '24 Girard St, Rochester, NY 14610',
-};
 
 beforeAll(async () => {
   browser = await chromium.launch({ headless: false });
@@ -32,8 +26,15 @@ it('As a user, I should be able to collapse the Elements container', async () =>
   expect(elementListClass).not.toContain('show');
 });
 
+const user = {
+  name: 'Test Tester',
+  email: 'test@test.com',
+  currentAddress: '3930 N Pine Grove Ave, Chicago, IL 60613',
+  permanentAddress: '24 Girard St, Rochester, NY 14610',
+};
+
 it('As a user, I should be able to submit valid data', async () => {
-  page.goto('https://www.demoqa.com/text-box');
+  await page.goto('https://www.demoqa.com/text-box');
   const userForm = await page.waitForSelector('#userForm');
   const usernameField = await userForm.$('#userName');
   const userEmailField = await userForm.$('#userEmail');
@@ -52,4 +53,39 @@ it('As a user, I should be able to submit valid data', async () => {
     const eleValue = await output.$eval(`#${key}`, (el) => el.innerText);
     expect(eleValue).toContain(value);
   }
+});
+
+it('As a user, I should receive an error when inputting invalid email', async () => {
+  await page.goto('https://www.demoqa.com/text-box');
+  const userForm = await page.waitForSelector('#userForm');
+  const userEmailField = await userForm.$('#userEmail');
+  await userEmailField.fill('test');
+  await page.click('#submit');
+  const emailClass = await userForm.$eval('#userEmail', (el) => el.className);
+  expect(emailClass).toContain('field-error');
+});
+
+const buttonTuple = [
+  ['Double Click', 'doubleClickMessage'],
+  ['Right Click', 'rightClickMessage'],
+  ['Click', 'dynamicClickMessage'],
+];
+
+buttonTuple.forEach(async ($type) => {
+  const [clickType, messageSelector] = $type;
+
+  it.only(`As a user, I should be able to ${clickType} an element`, async () => {
+    await page.goto('https://www.demoqa.com/buttons');
+
+    if (clickType === 'Double Click') {
+      await page.dblclick('#doubleClickBtn');
+    } else if (clickType === 'Right Click') {
+      await page.click('#rightClickBtn', { button: 'right' });
+    } else {
+      await page.click('button >> text="Click Me"');
+    }
+    const messageHandle = await page.$(`#${messageSelector}`);
+
+    expect(!!messageHandle).toBe(true);
+  });
 });
